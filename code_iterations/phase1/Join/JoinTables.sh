ac064dcdef721083a761e8207a4c262f37453f3e
Jiyuan <chestnutrupert@gmail.com>
2019-12-01 17:48:33 -0500

    Change relative path to absolute path on DUMBO

--- code_iterations/phase1/Join/JoinTables.sh
+++ code_iterations/phase1/Join/JoinTables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 impala-shell -i "compute-1-1" -f JoinTables.iql
-impala-shell -B -o ~/RBDA_Project_NYU_F19/phase1/data/phase1_joined.tsv -i "compute-1-1" -f GenerateOutput.iql
-hdfs dfs -put ~/RBDA_Project_NYU_F19/phase1/data/phase1_joined.tsv Final/phase1/Join/output
+impala-shell -B -o ~/big-data-project/code_iterations/phase1/data/phase1_joined.tsv -i "compute-1-1" -f GenerateOutput.iql
+hdfs dfs -put ~/big-data-project/code_iterations/phase1/data/phase1_joined.tsv /user/jl11046/Final/phase1/Join/output
